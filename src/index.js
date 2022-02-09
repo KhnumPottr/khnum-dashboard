3c78475dc4ed8c9279116c45291209401e203c76
Joseph Garner <joseph.peter.garner@gmail.com>
2022-02-09 21:24:47 +1100

    KP-14: switched code base over to JavaScript from TypeScript

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Dashboard from './pages/Dashboard';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <>
-    <Dashboard/>
-  </>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
