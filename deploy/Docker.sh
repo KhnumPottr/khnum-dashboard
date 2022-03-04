fda7026ab934767651f69cf93e9e2a04e37ba646
Joseph Garner <joseph.peter.garner@gmail.com>
2022-03-04 17:19:01 +1100

    KP-43: BugFix Mongo Data Lost

--- deploy/Docker.sh
+++ deploy/Docker.sh
@@ -1,3 +1,6 @@
+docker container stop KhnumDashboard
+docker image rm khnum-dashboard
+
 docker build -t khnum-dashboard .
 
-docker run -d -p 3000:3000 khnum-dashboard
+docker run -d -name KhnumDashboard -p 3000:3000 khnum-dashboard
